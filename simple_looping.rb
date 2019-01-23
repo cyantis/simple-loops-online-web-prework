ff03865b090bdc2dc84b092aeede0f1e4b7fa907
Cody Yantis <cyantis@gmail.com>
2019-01-23 17:43:27 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,8 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  for counter in (1..number_of_times)
+  for counter in 1..number_of_times do
     phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
   end
 end
